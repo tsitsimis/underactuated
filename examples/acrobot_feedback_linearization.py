a7fe3b1e212992c0811a2d973e0bb4ce6df8b921
Theodore Tsitsimis <Theodore.Tsitsimis@gr.ey.com>
2020-06-13 14:22:08 +0300

    Rename files

--- examples/acrobot_feedback_linearization.py
+++ examples/acrobot_feedback_linearization.py
@@ -1,18 +1,17 @@
-from systems.pendulum import Pendulum
+from systems.acrobot import Acrobot
 from controllers import FLC
 
 import numpy as np
 import matplotlib.pyplot as plt
 
-
 # Plant
-plant = Pendulum(mass=1, length=1, friction=0.5, gravity=10, x0=np.array([np.pi/4, -5]))
+plant = Acrobot(m1=1, m2=1, l1=1, l2=1, gravity=10, x0=np.array([np.pi/4, np.pi/4, 0, 0]))
 
 # Feedback Linearization Controller
 # Make system feedback-equivalent to a linear system
 # controlled with a simple PD controller
-plant.u = FLC(plant, lambda t, x: np.array([[-2*x[1] - 2*(x[0] - np.pi)]])).controller
+plant.u = FLC(plant, lambda t, x: np.array([[-(x[0] - np.pi) - x[2]], [-(x[1] - 0) - x[3]]])).controller
 
 # Animate
 fig, ax = plt.subplots(figsize=(5, 5))
-plant.playback(fig=fig, ax=ax, T=50)
+plant.playback(fig=fig, ax=ax, T=10)
