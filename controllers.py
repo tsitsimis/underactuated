6488deb87fd36f9eecec8070a2a9161744b541e6
Theodore Tsitsimis <Theodore.Tsitsimis@gr.ey.com>
2020-06-13 14:21:19 +0300

    Add LQR controller

--- controllers.py
+++ controllers.py
@@ -1,4 +1,5 @@
 import numpy as np
+from scipy.linalg import solve_continuous_are
 
 
 class FLC:
@@ -13,3 +14,16 @@ class FLC:
         M, M_inv, C, tau_g, B = self.plant.get_manipulator_matrices(x)
         u = np.linalg.inv(B).dot(M.dot(self.v(t, x)) + C.dot(q_dot) - tau_g)
         return u
+
+
+class LQR:
+    def __init__(self, A, B, Q, R):
+        self.A = A
+        self.B = B
+        self.Q = Q
+        self.R = R
+
+        self.S = solve_continuous_are(A, B, Q, R)
+
+    def controller(self, t, x):
+        return -np.linalg.inv(self.R).dot(self.B.T).dot(self.S).dot(x[:, None])
