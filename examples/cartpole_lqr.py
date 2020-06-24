1f7455ac9e5ae73e08d05aaec282cf090cb4465c
Theodore Tsitsimis <Theodore.Tsitsimis@gr.ey.com>
2020-06-24 12:10:04 +0300

    Apply LQR to underactuated robot

--- examples/cartpole_lqr.py
+++ examples/cartpole_lqr.py
@@ -7,16 +7,21 @@ import matplotlib.pyplot as plt
 
 
 # Plant
-plant = CartPole(m_c=1, m_p=1, l=1, gravity=10, x0=np.array([-2, np.pi*3/4, 0, 0]), underactuated=False)
+plant = CartPole(m_c=1, m_p=1, l=1, gravity=10, x0=np.array([-2, np.pi*3/4, 0, 0]), underactuated=True)
 
 # Linearize around fixed point (vertical position, zero velocity)
 x_goal = np.array([0, np.pi, 0, 0])
-sl = SystemLinearizer(plant, x0=x_goal, tau_g_dq=np.array([[0, 0], [0, 10]]))
+m_p, l, g = plant.m_p, plant.l, plant.gravity
+tau_g_dq = np.array([
+    [0, 0],
+    [0, m_p * l * g]
+])
+sl = SystemLinearizer(plant, x0=x_goal, tau_g_dq=tau_g_dq)
 
 # LQR controller
-lqr = LQR(A=sl.A_lin, B=sl.B_lin, Q=10*np.eye(4), R=np.eye(2))
+lqr = LQR(A=sl.A_lin, B=sl.B_lin, Q=10*np.eye(4), R=np.eye(1))
 plant.u = lambda t, x: lqr.controller(t, x - x_goal)
 
 # Animate
 fig, ax = plt.subplots(figsize=(5, 5))
-plant.playback(fig=fig, ax=ax, T=4, save=True)
+plant.playback(fig=fig, ax=ax, T=10, save=True)
