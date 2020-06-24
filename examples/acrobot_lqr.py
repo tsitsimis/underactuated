90ac4d12121e7ada08f8f6c55c38e1bc0f40c3d0
Theodore Tsitsimis <Theodore.Tsitsimis@gr.ey.com>
2020-06-24 12:04:17 +0300

    Apply LQR to underactuated robot

--- examples/acrobot_lqr.py
+++ examples/acrobot_lqr.py
@@ -5,19 +5,22 @@ from controllers import LQR
 import numpy as np
 import matplotlib.pyplot as plt
 
-
 # Plant
-plant = Acrobot(m1=1, m2=1, l1=1, l2=1, gravity=10, x0=np.array([np.pi*3/4, 0, 0, 0]), underactuated=False)
+plant = Acrobot(m1=1, m2=2, l1=1, l2=2, gravity=10, x0=np.array([np.pi * 0.8, 0, 0, 0]), underactuated=True)
 
 # Linearize around fixed point (vertical position, zero velocity)
 x_goal = np.array([np.pi, 0, 0, 0])
-sl = SystemLinearizer(plant, x0=x_goal, tau_g_dq=np.array([[20, 5], [5, 5]]))
+m1, m2, l1, l2, lc1, lc2, g = plant.m1, plant.m2, plant.l1, plant.l2, plant.lc1, plant.lc2, plant.gravity
+tau_g_dq = np.array([
+    [g * (m1 * lc1 + m2 * l1 + m2 * lc2), m2 * g * lc2],
+    [m2 * g * lc2, m2 * g * lc2]
+])
+sl = SystemLinearizer(plant, x0=x_goal, tau_g_dq=tau_g_dq)
 
 # LQR controller
-lqr = LQR(A=sl.A_lin, B=sl.B_lin, Q=10*np.eye(4), R=np.eye(2))
+lqr = LQR(A=sl.A_lin, B=sl.B_lin, Q=np.eye(4), R=np.eye(1))
 plant.u = lambda t, x: lqr.controller(t, x - x_goal)
 
 # Animate
 fig, ax = plt.subplots(figsize=(5, 5))
-plant.playback(fig=fig, ax=ax, T=4, save=True)
-
+plant.playback(fig=fig, ax=ax, T=10, save=True)
